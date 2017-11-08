89d63199f4ea108d5fad17e98cd4187e78b6bbad
Ekaterina <ekaterina17563@gmail.com>
2017-11-09 01:02:49 +0300

    add change color tshirt js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,3 +6,21 @@
 
     alert (message);
 })();
+
+const colors = {
+    'white': 'Белая',
+    'yellow': 'Желтая',
+    'green': 'Зеленая'
+};
+
+document.querySelector('.color').addEventListener('click', function (event) {
+    if (event.target.classList.contains('color__item')) {
+        const color = event.target.querySelector('input').value;
+        document.querySelector('.product__pic').setAttribute('src', 'img/tshirt_' + color + '.jpg');
+        document.querySelector('.product__pic').setAttribute('alt', colors[color] + ' футболка');
+        document.querySelector('.product__pic').setAttribute('title', colors[color] + ' футболка');
+        document.querySelector('.color__item.color__item_active').classList.remove('color__item_active');
+        event.target.classList.add('color__item_active');
+        event.target.querySelector('input').checked = true;
+    }
+});
